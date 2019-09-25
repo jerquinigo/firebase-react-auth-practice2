6ef4e8b49922585c656502d9264f3c26b88ec578
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-09-25 13:45:15 -0400

    final commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,7 +3,7 @@ import "firebase/auth";
 
 // Use actual config values from registered firbase app
 var config = {
-  apiKey: ,
+  apiKey: "AIzaSyD8Qrs2vAVXNdHuG6k1fpcVB4gYik9qXlI",
   authDomain: "fir-react-auth-practice.firebaseapp.com",
   databaseURL: "https://fir-react-auth-practice.firebaseio.com",
   projectId: "fir-react-auth-practice",
